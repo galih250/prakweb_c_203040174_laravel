0129055e41f539e5a8baa98d42149d1cbc23ed23
galih250 <galihk250@gmail.com>
2022-10-17 21:07:57 +0700

    menyelesaikan vidio 3 Struktur Folder, Routes & View

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,17 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/about', function () {
-    return view('about', [
-        "name" => "Linggih Kusumah Dilagga",
-        "email" => "GalihK250@gmail.com",
-        "image" => "profile.jpeg"
-    ]);
-});
-
-Route::get('/blog', function () {
-    return view('posts');
+    return view('welcome');
 });
