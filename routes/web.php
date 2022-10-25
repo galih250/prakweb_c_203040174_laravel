5652a77417299d87ce893ff22b497984ae4ecdfa
galih250 <galihk250@gmail.com>
2022-10-25 18:55:57 +0700

    Vidio 8 Category & Relationships

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
-
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,20 +15,35 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('home', [
         "title" => "Home"
     ]);
 });
-
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
         "name" => "Linggih Kusumah Dilagga",
-        "email" => "Galihk250@gmai.com",
+        "email" => "Galihk250@gmail.com",
         "image" => "profile.jpeg"
     ]);
 });
-Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/blog', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
